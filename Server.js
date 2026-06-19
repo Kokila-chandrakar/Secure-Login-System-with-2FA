40407f3fab7b769cced3f31d192d7b26b5daaa4d
Kokila-chandrakar <payalchandrakar327@gmail.com>
2026-06-20 02:21:30 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -6,3 +6,6 @@ const speakeasy = require('speakeasy');
 const QRCode = require('qrcode');
 const session = require('express-session');
 const path = require('path');
+
+const app = express();
+const port = 3000;
