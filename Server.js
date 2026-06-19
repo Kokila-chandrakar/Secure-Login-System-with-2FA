77c60b9e85ea168369eff546fea9f1e6ca72a524
Kokila-chandrakar <payalchandrakar327@gmail.com>
2026-06-20 02:21:02 +0530

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,8 @@
+// server.js - Main backend server (Express + SQLite)
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const bcrypt = require('bcrypt');
+const speakeasy = require('speakeasy');
+const QRCode = require('qrcode');
+const session = require('express-session');
+const path = require('path');
